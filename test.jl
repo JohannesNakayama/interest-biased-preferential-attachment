66fbf16d1b7919119493e550e7827c890724f8d1
JohannesNakayama <38665199+JohannesNakayama@users.noreply.github.com>
2021-11-01 17:23:17 +0100

    Summarized script in module, refactored code a bit
    The module is now usable for numerical analysis

--- test.jl
+++ test.jl
@@ -1,7 +1,86 @@
+module IBPA
+
+# Dependencies
 using Graphs
 using Random
 using StatsBase
 using UnicodePlots
+
+# User API
+export IBPAModel
+export IBPANetwork
+export sample
+
+# Interest-biased preferential attachment model
+mutable struct IBPAModel
+    n::Int
+    m_0::Int
+    m::Int
+    n_interests::Int
+    self_weight::Float64
+end
+
+# Interest-biasel preferential attachment network (instantiance of the above model)
+mutable struct IBPANetwork
+    g::AbstractGraph
+    node_interest_map::Dict
+end
+
+# Sample from a given IBPA model
+function sample(model::IBPAModel, samples)
+    samp = [evolve_model(model) for i in 1:samples]
+    return samp
+end
+
+# Evolve a given model
+function evolve_model(model)
+
+    g = Graphs.SimpleGraph(model.m_0)
+    node_interest_map = Dict([(i, Random.rand(1:model.n_interests)) for i in 1:nv(g)])
+
+    for new_node in (model.m_0 + 1):model.n
+
+        node_degrees = degree(g)
+
+        new_node_interest = Random.rand(1:model.n_interests)
+
+        interest_weights = calculate_interest_weights(g, node_interest_map, new_node_interest, model.n_interests, model.self_weight)
+
+        node_weights = sum(node_degrees) == 0 ? repeat([1 / nv(g)], nv(g)) : node_degrees / sum(node_degrees)
+        interest_weights /= sum(interest_weights)
+
+        weights = (node_weights + interest_weights) / 2
+        samp = StatsBase.sample(1:nv(g), Weights(weights), model.m, replace = false)
+
+        add_vertex!(g)
+        for att in samp
+            add_edge!(g, (nv(g), att))
+        end
+
+        node_interest_map[nv(g)] = new_node_interest
+
+    end
+
+    return IBPANetwork(g, node_interest_map)
+end
+
+# Calculate the weights for each node given its interest
+function calculate_interest_weights(g::AbstractGraph, node_interest_map, new_node_interest, n_interests, self_weight)
+    interest_weights = ones(nv(g))
+
+    for k in keys(node_interest_map)
+        if node_interest_map[k] == new_node_interest
+            interest_weights[k] = self_weight
+        else
+            interest_weights[k] = (1 - self_weight) / (n_interests - 1)
+        end
+    end
+
+    return interest_weights
+end
+
+end  # end module
+
 
 # create m_0 initial nodes
 #
@@ -15,49 +94,14 @@ using UnicodePlots
 # the preferential attachment is then not only weighted by the existing nodes' degree, but also by the interest of a connecting node
 # each node has one of the interests represented in the network
 
-n = 1000
-m_0 = 10
-m = 3
-n_interests = 3
-# own_interest_weight = 1 / n_interests  # default: BA equivalent
-own_interest_weight = 0.5
-
-G = SimpleGraph(m_0)
-node_interest_map = Dict([(i, Random.rand(1:n_interests)) for i in 1:10])
-
-for new_node in (m_0 + 1):n
-
-    node_degrees = degree(G)
-
-    new_node_interest = Random.rand(1:n_interests)
-
-    interest_weights = ones(nv(G))
-
-    for k in keys(node_interest_map)
-        if node_interest_map[k] == new_node_interest
-            interest_weights[k] = own_interest_weight
-        else
-            interest_weights[k] = (1 - own_interest_weight) / (n_interests - 1)
-        end
-    end
-
-    node_weights = sum(node_degrees) == 0 ? repeat([1 / nv(G)], nv(G)) : node_degrees / sum(node_degrees)
-    interest_weights /= sum(interest_weights)
-
-    weights = (node_weights + interest_weights) / 2
-    samp = StatsBase.sample(1:nv(G), Weights(weights), m, replace = false)
-
-    add_vertex!(G)
-    for att in samp
-        add_edge!(G, (nv(G), att))
-    end
-
-    node_interest_map[nv(G)] = new_node_interest
-
-end
+# self_weight = 1 / n_interests  # default: BA equivalent
 
 
-histogram(degree(G), bins = 50)
+
+
+
+
+
 
 
 
