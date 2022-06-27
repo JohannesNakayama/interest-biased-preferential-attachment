4944b3a8dddcdeac11a94bb5c876c545d027587c
JohannesNakayama <38665199+JohannesNakayama@users.noreply.github.com>
2022-06-27 17:56:48 +0200

    Added some notes

--- test.jl
+++ test.jl
@@ -29,6 +29,7 @@ mutable struct IBPANetwork
 end
 
 # Sample from a given IBPA model
+# TODO: dispatch from Random.sample
 function sample(model::IBPAModel, samples)
     samp = [run_model(model) for i in 1:samples]
     return samp
@@ -53,13 +54,14 @@ function add_ibpa_node!(g, node_interest_map, model)
     new_node_interest = Random.rand(1:model.n_interests)
 
     # Calculate weights
-    interest_weights = calculate_interest_weights(g, 
-                                                  node_interest_map, 
-                                                  new_node_interest, 
-                                                  model.n_interests, 
+    interest_weights = calculate_interest_weights(g,
+                                                  node_interest_map,
+                                                  new_node_interest,
+                                                  model.n_interests,
                                                   model.self_weight)
     node_weights = sum(node_degrees) == 0 ? repeat([1 / nv(g)], nv(g)) : node_degrees / sum(node_degrees)
     weights = (node_weights + interest_weights) / 2
+    # TODO: implement more elegantly
 
     # Choose nodes to attach to
     samp = StatsBase.sample(1:nv(g), Weights(weights), model.m, replace = false)
@@ -77,10 +79,10 @@ end
 
 
 # Calculate the weights for each node given its interest
-function calculate_interest_weights(g::AbstractGraph, 
-                                    node_interest_map, 
-                                    new_node_interest, 
-                                    n_interests, 
+function calculate_interest_weights(g::AbstractGraph,
+                                    node_interest_map,
+                                    new_node_interest,
+                                    n_interests,
                                     self_weight)
     interest_weights = ones(nv(g))
 
